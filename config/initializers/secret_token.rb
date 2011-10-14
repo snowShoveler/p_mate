ffa438408ac5b58923d85a329a983d54d2112c9d
Chris Plawchan <cjplawc@gmail.com>
2011-10-13 21:46:46 -0400

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PMate::Application.config.secret_token = 'ff7e26e723b3b116748ffe217abb2ebe7f264004822e4e022f065098cb6bc438f7a5a7a79b438168300966cbcb1d940b20a30a14ce84ab4369f523adcb6da669'
